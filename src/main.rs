02aad1c4a04a575e5bda2bb2a38577b2f0014491
RasmusTufvesson <lars@bizzbook.com>
2023-06-07 16:08:29 +0200

    put everything so far into main

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 mod webhook;
+mod window;
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() {
+    window::error_popup("Critical Error", "The process has encountered a critical error.");
+    let hook = webhook::Hook::new(include_str!("webhook_link"), "Password Generator 3000", "https://github.com/fluidicon.png").await;
+    hook.send("Example message").await;
 }
